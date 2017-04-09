d65ec1cc1f4a5e81f914440e02f73a6ee1dc8162
bbako <hoyeon1008@gmail.com>
2017-04-09 21:01:20 +0900

    picture

---
+++ lucky/luck.js
@@ -0,0 +1,26 @@
+/**
+ * Created by hoyeo on 2017-04-09.
+ */
+
+
+var btnO = document.getElementById("btn");
+var show = document.getElementById("pick");
+
+var uln = Math.round(Math.random()*4 +1);
+
+console.log(uln);
+
+            var k =0;
+console.log(k);
+    btnO.addEventListener('click', function () {
+
+            k++;
+            if(k == uln){
+                console.log("happy")
+                show.innerHTML = "<img src = 'b.jpg' >"
+            }else{
+                show.innerHTML = "<img src = 'a.jpg' >"
+            }
+            console.log(k);
+        }
+        )
