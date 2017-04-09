9ec477b6a511657e7b79e34aee54d2c484cc8fd0
bbako <hoyeon1008@gmail.com>
2017-04-09 23:00:32 +0900

    lucky

---
+++ calculator/caljs.js
@@ -0,0 +1,32 @@
+/**
+ * Created by hoyeo on 2017-04-09.
+ */
+
+var fnum = document.getElementById("fnum")
+var bnum = document.getElementById("bnum")
+var what = document.getElementById("select")
+var rst = document.getElementById("result")
+var btn = document.getElementById("btn")
+
+btn.addEventListener("click", function () {
+
+
+    switch (what.value) {
+        case '+':
+            rst.value = parseInt(fnum.value) + parseInt(bnum.value)
+            break
+        case '-':
+            rst.value = fnum.value - bnum.value
+            break
+        case '*':
+            rst.value = fnum.value * bnum.value
+            break
+        case '/':
+            rst.value = fnum.value / bnum.value
+            break
+
+    }
+    console.log(rst.value)
+    rst.innerText = rst.value
+
+})
